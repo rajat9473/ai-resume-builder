8f15888244d6fc70d71322b57d4ea272a4141e40
Rajat Chitransh <rajatchitransh02@gmail.com>
2026-06-10 02:27:06 +0530

    mongodb atlas configured

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(
+      process.env.MONGODB_URI
+    );
 
-    console.log("MongoDB Connected");
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`
+    );
   } catch (error) {
-    console.error("MongoDB Error:", error.message);
+    console.error(error);
     process.exit(1);
   }
 };
